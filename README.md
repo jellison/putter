f3a512286509a4a0656bf1c097b55263d1469019
Justin Ellison <jellison@sitepen.com>
2019-07-20 23:26:08 -0400

    Setup Typescript / Webpack / Electron / React

---
+++ README.md
@@ -0,0 +1,21 @@
+# Putter
+
+## Scripts
+
+### Install dependencies
+
+```
+npm install
+```
+
+### Build for local development
+
+```
+npm run build
+```
+
+### Run linter (tslint)
+
+```
+npm run lint
+```
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const outputPath = __dirname + '/dist';
+const rules = [
+  {
+    test: /\.ts(x?)$/,
+    exclude: /node_modules/,
+    use: [
+      {
+        loader: 'ts-loader'
+      }
+    ]
+  },
+  {
+    enforce: 'pre',
+    test: /\.js$/,
+    loader: 'source-map-loader'
+  }
+];
+
+module.exports = [
+  // Electron Main
+  {
+    mode: 'development',
+    entry: './src/electron.ts',
+    target: 'electron-renderer',
+    devtool: 'source-map',
+    target: 'electron-main',
+    module: { rules },
+    output: {
+      path: outputPath,
+      filename: 'electron.js'
+    }
+  },
+  // Renderer (React)
+  {
+    mode: 'development',
+    entry: './src/app.tsx',
+    devtool: 'source-map',
+    module: { rules },
+    output: {
+      path: outputPath,
+      filename: 'bundle.js'
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/app.html'
+      })
+    ]
+  }
+];
